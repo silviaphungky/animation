43abd17f36d449b3fa07ab6921bd11dcfd93e32c
silviaphungky <silvia.phungky@mandirisek.co.id>
2024-06-27 23:53:39 +0700

    add suspense

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,7 @@ import { usernameKey } from '@/constants/key'
 import { PATHS } from '@/constants/paths'
 
 import { useRouter, useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 
 const Login = () => {
@@ -23,45 +24,47 @@ const Login = () => {
   }
 
   return (
-    <div className="relative h-screen bg-gray-50">
-      <div className="absolute left-0 top-[3rem]">
-        <IconBatik />
-      </div>
-      <div className="absolute right-0 top-[3rem]">
-        <IconBatik />
-      </div>
-
-      <form
-        className="absolute left-0 right-0 m-auto h-screen w-[450px] p-4 flex items-center justify-center z-10"
-        onSubmit={handleSubmit(handleLogin)}
-      >
-        <div>
-          <div className="text-3xl font-bold text-center mb-8">
-            Sign in to your account
-          </div>
-          <div className="mb-2 z-20">
-            <Controller
-              name={'username'}
-              control={control}
-              rules={{ required: 'Required' }}
-              render={({ field, fieldState: { error } }) => {
-                const errorMsg = error?.message
-                return (
-                  <FormGroup label="Username" error={errorMsg || ''}>
-                    <Input placeholder="Username" type="text" {...field} />
-                  </FormGroup>
-                )
-              }}
-            />
-          </div>
-          <div className="w-full mt-4">
-            <Button type="submit" full variant="primary">
-              Sign in
-            </Button>
-          </div>
+    <Suspense>
+      <div className="relative h-screen bg-gray-50">
+        <div className="absolute left-0 top-[3rem]">
+          <IconBatik />
         </div>
-      </form>
-    </div>
+        <div className="absolute right-0 top-[3rem]">
+          <IconBatik />
+        </div>
+
+        <form
+          className="absolute left-0 right-0 m-auto h-screen w-[450px] p-4 flex items-center justify-center z-10"
+          onSubmit={handleSubmit(handleLogin)}
+        >
+          <div>
+            <div className="text-3xl font-bold text-center mb-8">
+              Sign in to your account
+            </div>
+            <div className="mb-2 z-20">
+              <Controller
+                name={'username'}
+                control={control}
+                rules={{ required: 'Required' }}
+                render={({ field, fieldState: { error } }) => {
+                  const errorMsg = error?.message
+                  return (
+                    <FormGroup label="Username" error={errorMsg || ''}>
+                      <Input placeholder="Username" type="text" {...field} />
+                    </FormGroup>
+                  )
+                }}
+              />
+            </div>
+            <div className="w-full mt-4">
+              <Button type="submit" full variant="primary">
+                Sign in
+              </Button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </Suspense>
   )
 }
 
