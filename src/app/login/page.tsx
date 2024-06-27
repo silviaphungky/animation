9e09f0b06f0392e64146a1584af9b4cc6fddb1a3
silviaphungky <silvia.phungky@mandirisek.co.id>
2024-06-27 17:28:15 +0700

    fix build error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ import { IconBatik } from '@/components/Icons'
 import { usernameKey } from '@/constants/key'
 import { PATHS } from '@/constants/paths'
 
-import { useRouter, useSearchParams } from 'next/navigation'
+import { useParams, useRouter } from 'next/navigation'
 import { Controller, useForm } from 'react-hook-form'
 
 const Login = () => {
@@ -14,8 +14,7 @@ const Login = () => {
       username: '',
     },
   })
-  const params = useSearchParams()
-  const callback = params.get('callback')
+  const { callback } = useParams()
 
   const handleLogin = (value: { username: string }) => {
     router.push(callback ? callback : PATHS.HOME)
