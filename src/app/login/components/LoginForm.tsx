9c99cb564c8751e76af435fbe0a65c647d5138a6
silviaphungky <silvia.phungky@mandirisek.co.id>
2024-06-28 12:11:54 +0700

    suspense in parent

--- src/app/login/components/LoginForm.tsx
+++ src/app/login/components/LoginForm.tsx
@@ -25,38 +25,36 @@ const LoginForm = () => {
   }
 
   return (
-    <Suspense>
-      <form
-        className="absolute left-0 right-0 m-auto h-screen w-[450px] p-4 flex items-center justify-center z-10"
-        onSubmit={handleSubmit(handleLogin)}
-      >
-        <div>
-          <div className="text-3xl font-bold text-center mb-8">
-            Sign in to your account
-          </div>
-          <div className="mb-2 z-20">
-            <Controller
-              name={'username'}
-              control={control}
-              rules={{ required: 'Required' }}
-              render={({ field, fieldState: { error } }) => {
-                const errorMsg = error?.message
-                return (
-                  <FormGroup label="Username" error={errorMsg || ''}>
-                    <Input placeholder="Username" type="text" {...field} />
-                  </FormGroup>
-                )
-              }}
-            />
-          </div>
-          <div className="w-full mt-4">
-            <Button type="submit" full variant="primary">
-              Sign in
-            </Button>
-          </div>
+    <form
+      className="absolute left-0 right-0 m-auto h-screen w-[450px] p-4 flex items-center justify-center z-10"
+      onSubmit={handleSubmit(handleLogin)}
+    >
+      <div>
+        <div className="text-3xl font-bold text-center mb-8">
+          Sign in to your account
         </div>
-      </form>
-    </Suspense>
+        <div className="mb-2 z-20">
+          <Controller
+            name={'username'}
+            control={control}
+            rules={{ required: 'Required' }}
+            render={({ field, fieldState: { error } }) => {
+              const errorMsg = error?.message
+              return (
+                <FormGroup label="Username" error={errorMsg || ''}>
+                  <Input placeholder="Username" type="text" {...field} />
+                </FormGroup>
+              )
+            }}
+          />
+        </div>
+        <div className="w-full mt-4">
+          <Button type="submit" full variant="primary">
+            Sign in
+          </Button>
+        </div>
+      </div>
+    </form>
   )
 }
 
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,21 @@
 'use client'
 import { IconBatik } from '@/components/Icons'
 import { LoginForm } from './components'
+import { Suspense } from 'react'
 
 const Login = () => {
   return (
-    <div className="relative h-screen bg-gray-50">
-      <div className="absolute left-0 top-[3rem]">
-        <IconBatik />
+    <Suspense>
+      <div className="relative h-screen bg-gray-50">
+        <div className="absolute left-0 top-[3rem]">
+          <IconBatik />
+        </div>
+        <div className="absolute right-0 top-[3rem]">
+          <IconBatik />
+        </div>
+        <LoginForm />
       </div>
-      <div className="absolute right-0 top-[3rem]">
-        <IconBatik />
-      </div>
-      <LoginForm />
-    </div>
+    </Suspense>
   )
 }
 
